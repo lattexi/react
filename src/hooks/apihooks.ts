a0234c4ae339514956d24689720d28716520261e
lattexi <laurikarhu@outlook.com>
2025-01-29 10:34:43 +0200

    useMedia hook

--- src/hooks/apihooks.ts
+++ src/hooks/apihooks.ts
@@ -1,16 +1,60 @@
-const useMedia() => {
-    const [media, setMedia] = useState([]);
-    const [loading, setLoading] = useState(false);
-    
+import { MediaItem, MediaItemWithOwner, UserWithNoPassword } from "hybrid-types/DBTypes";
+import { useEffect, useState } from "react";
+import { fetchData } from "../lib/functions";
+
+const useMedia = () => {
+    const [mediaArray, setMediaArray] = useState<MediaItemWithOwner[]>([]);
+
     useEffect(() => {
-        setLoading(true);
-        fetch('https://api.example.com/media')
-        .then(response => response.json())
-        .then(data => {
-            setMedia(data);
-            setLoading(false);
-        });
-    }, []);
+        const getMedia = async () => {
+          try {
+            const media = await fetchData<MediaItem[]>(
+              import.meta.env.VITE_MEDIA_API + "/media",
+            );
+            const mediaWithOwner: MediaItemWithOwner[] = await Promise.all(
+              media.map(async (item) => {
+                const owner = await fetchData<UserWithNoPassword>(
+                  import.meta.env.VITE_AUTH_API + "/users/" + item.user_id,
+                );
     
-    return { media, loading };
-    };
+                const mediaItem: MediaItemWithOwner = {
+                  ...item,
+                  username: owner.username,
+                };
+    
+                if (
+                  mediaItem.screenshots &&
+                  typeof mediaItem.screenshots === "string"
+                ) {
+                  mediaItem.screenshots = JSON.parse(mediaItem.screenshots).map(
+                    (screenshot: string) => {
+                      return import.meta.env.VITE_FILE_URL + screenshot;
+                    },
+                  );
+                }
+                return mediaItem;
+              }),
+            );
+            setMediaArray(mediaWithOwner);
+          } catch (e) {
+            console.error((e as Error).message);
+          }
+        };
+        getMedia();
+      }, []);
+    
+
+  return {mediaArray};
+}
+
+const useUser = () => {
+    // TODO: implement auth/user server api call
+    
+}
+
+const useComments = () => {
+    // TODO: implement comments server api call
+    
+}
+
+export { useMedia, useUser, useComments };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,56 +1,15 @@
-import {
-  MediaItem,
-  MediaItemWithOwner,
-  UserWithNoPassword,
-} from "hybrid-types/DBTypes";
+import { MediaItemWithOwner } from "hybrid-types/DBTypes";
 import MediaRow from "../components/MediaRow";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SingleView from "../components/SingleView";
-import { fetchData } from "../lib/functions";
+import { useMedia } from "../hooks/apihooks";
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState<
     MediaItemWithOwner | undefined
   >(undefined);
-  const [mediaArray, setMediaArray] = useState<MediaItemWithOwner[]>([]);
 
-  useEffect(() => {
-    const getMedia = async () => {
-      try {
-        const media = await fetchData<MediaItem[]>(
-          import.meta.env.VITE_MEDIA_API + "/media",
-        );
-        const mediaWithOwner: MediaItemWithOwner[] = await Promise.all(
-          media.map(async (item) => {
-            const owner = await fetchData<UserWithNoPassword>(
-              import.meta.env.VITE_AUTH_API + "/users/" + item.user_id,
-            );
-
-            const mediaItem: MediaItemWithOwner = {
-              ...item,
-              username: owner.username,
-            };
-
-            if (
-              mediaItem.screenshots &&
-              typeof mediaItem.screenshots === "string"
-            ) {
-              mediaItem.screenshots = JSON.parse(mediaItem.screenshots).map(
-                (screenshot: string) => {
-                  return import.meta.env.VITE_FILE_URL + screenshot;
-                },
-              );
-            }
-            return mediaItem;
-          }),
-        );
-        setMediaArray(mediaWithOwner);
-      } catch (e) {
-        console.error((e as Error).message);
-      }
-    };
-    getMedia();
-  }, []);
+  const { mediaArray } = useMedia();
 
   console.log(mediaArray);
   return (
